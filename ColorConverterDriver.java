e1e3b10e8de004c6c4ff5b246a87c2e68fc79cc0
esha0612 <73080687+esha0612@users.noreply.github.com>
2024-02-08 21:40:25 -0800

    Update ColorConverterDriver.java

--- ColorConverterDriver.java
+++ ColorConverterDriver.java
@@ -1,20 +1,46 @@
-package color;
+package ColorPackage;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-public class ColorConverterDriver {
-public static void main(String[] args) {
-		int [] red = new int[]
-	ColorConverter obj1 = new ColorConverter(1, 0, 255, 255);
-		Color obj2 = new Color(1, 0, 255, 0);
-		Color obj3 = new Color(1, 0, 0, 255);
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ColorConverterDriver extends JPanel {
+	
+	public void paintComponent(Graphics g) {
 		
-		int[] hsv1 = obj1.getRGB();
-		int[] hsv2 = obj2.getRGB();
-		int[] hsv3 = obj3.getRGB();
-		System.out.println(hsv1);
-		System.out.println(hsv2);
-		System.out.println(hsv3);
-		
+	    super.paintComponent(g);
+	    Graphics2D g2d = (Graphics2D) g;
+
+	    g2d.setColor(new Color(1, 0, 255, 255));
+	    g2d.drawRect(10, 15, 90, 60);
+
+
+	    g2d.setColor(new Color(1, 0, 255, 255));
+	    g2d.fillRect(250, 195, 90, 60);
+
+	  }
+	public static void main(String[] args) {
+		int[] first = {0, 255, 255, 0, 0, 255, 0, 255, 192, 128, 128, 128, 0, 128, 0, 0};
+	    int[] second = {0, 255, 0, 255, 0, 255, 255, 0, 192, 128, 0, 128, 128, 0, 128, 0};
+	    int[] third = {0, 255, 0, 0, 255, 0, 255, 255, 192, 128, 0, 0, 0, 128, 128, 128};
+        ColorConverter cc = new ColorConverter(4, 4, 1, first, second, third);
+        cc.print();
+        System.out.println("HSV:");
+        cc.RGBtoHSV();
+        cc.print();
+        System.out.println("RGB");
+        cc.HSVtoRGB();
+        cc.print();
+        
+        ColorConverterDriver rects = new ColorConverterDriver();
+        JFrame frame = new JFrame("Rectangles");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(rects);
+        frame.setSize(250, 250);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
 	}
 }
-
-
