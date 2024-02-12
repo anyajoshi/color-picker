b73d132474e2439aff9039f5eb3ade2a6ae6ca62
anyajoshi <72575786+anyajoshi@users.noreply.github.com>
2024-02-11 18:11:28 -0800

    Update ColorV3.java

--- ColorV3.java
+++ ColorV3.java
@@ -1,347 +1,200 @@
-public class Color {
-    private final int MAX_COLOR = 255;
-    private int red;
-    private int green;
-    private int blue;
-    private int hue;
-    private int saturation;
-    private int brightness;
-    /**
-     * @author Anya Joshi
-     * @param type
-     * @param value1
-     * @param value2
-     * @param value3
-     */
-    public Color(int type, int value1, int value2, int value3) {
-        if (type == 1) {
-            setRGB(value1, value2, value3);
-        } else if (type == 2) {
-            setHSV(value1, value2, value3);
+import java.io.File;
+import java.util.Arrays;
+import java.util.Random;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ColorConverterDriver extends JPanel {
+
+    private Color[][] rectColors;
+
+    public void paintComponent(Graphics g) {
+        Random rand = new Random();
+        int a =0;
+        int b= 0;
+        int c = 60;
+        int d = 60;
+
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+	  /*  g2d.setColor(new Color(1, 0, 255, 255));
+	    g2d.fillRect(0, 0, 20, 20); //(a , b, how wide it is, top to bottom (how long)
+	    g2d.setColor(new Color(1, 255, 0, 255));
+	    g2d.fillRect(0, 20, 20, 20); //(translate left/right, translate down, how wide it is, top to bottom (how long)
+	    g2d.setColor(new Color(1, 0, 0, 255));
+	    g2d.fillRect(20, 20, 20, 20);//translate to the right
+	    g2d.setColor(new Color(1, 200, 150, 255));
+	    g2d.fillRect(40, 20, 20, 20);*/
+
+        g2d.setColor(new java.awt.Color(255, 255, 0));
+        g2d.fillRect(40, 20, 40, 40);
+        for (int row=0; row< rectColors.length; row++) {
+            for (int col = 0; col < rectColors[row].length; col++) {
+                java.awt.Color gridColor = new java.awt.Color(rectColors[row][col].getRed(),rectColors[row][col].getGreen(),rectColors[row][col].getBlue());
+                g2d.setColor(gridColor) ;
+                g2d.fillRect(a, b, c, d);
+                a+=60;
+            }
+            a=0;
+            b+=60;
         }
-    }
-
-    /**
-     * @author Anya Joshi
-     * @param r
-     * @param g
-     * @param b
-     */
-    public void setRGB(int r, int g, int b) {
-        red = r;
-        green = g;
-        blue = b;
-
-    }
-    /**
-     * @author Anya Joshi
-     * @param h
-     * @param s
-     * @param v
-     */
-    public void setHSV(int h, int s, int v) {
-        hue = h;
-        saturation = s;
-        brightness = v;
-
-
-
-    }
-    /**
-     * @author Anya Joshi
-     * @param r
-     */
-    public void setRed(int r) {
-        red = r;
-
-
-    }
-    /**
-     * @author Anya Joshi
-     * @param g
-     */
-    public void setGreen(int g) {
-        green = g;
-
-
-    }
-    /**
-     * @author Anya Joshi
-     * @param b
-     */
-    public void setBlue(int b) {
-        blue = b;
-
-
-    }
-    /**
-     * @author Anya Joshi
-     * @param h
-     */
-    public void setHue(int h) {
-        hue = h;
-
-
-    }
-    /**
-     * @author Anya Joshi
-     * @param s
-     */
-    public void setSaturation(int s) {
-        saturation = s;
-
-
-    }
-    /**
-     * @author Anya Joshi
-     * @param v
-     */
-    public void setBrightness(int v) {
-        brightness = v;
-
 
     }
 
-    /**
-     * @author Anya Joshi
-     * @return
-     */
-    public int getRed(){
-        return red;
-    }
 
-    public int getBlue(){
-        return blue;
-    }
-    public int getGreen(){
-        return green;
-    }
-    public int getHue(){
-        return hue;
-    }
-    public int getSaturation(){
-        return saturation;
-    }
-    public int getBrightness(){
-        return brightness;
-    }
 
-    public int[] getRGB(){
-        return new int[]{red,green,blue};
-    }
+    public static void main(String[] args) {
 
-    public int[] getHSV(){
-        return new int[]{hue,saturation,brightness};
-    }
+        //Setup RGB with given values
+        int[] first = {0, 255, 255, 0, 0, 255, 0, 255, 192, 128, 128, 128, 0, 128, 0, 0};
+        int[] second = {0, 255, 0, 255, 0, 255, 255, 0, 192, 128, 0, 128, 128, 0, 128, 0};
+        int[] third = {0, 255, 0, 0, 255, 0, 255, 255, 192, 128, 0, 0, 0, 128, 128, 128};
+        ColorConverter cc = new ColorConverter(4, 4, 1, first, second, third);
+        //cc.print();
 
-    private double getMaximum(double d1, double d2, double d3) {
-        if (d1 >= d2 && d1 >= d3) {
-            return d1;
-        } else if (d2 >= d1 && d2 >= d3) {
-            return d2;
+        System.out.println("==============");
+        System.out.println("== Given RGB to HSV ==");
+        System.out.println("==============");
+        cc.RGBtoHSV();
+        cc.print();
+
+        System.out.println("==============");
+        System.out.println("== Given HSV back to RGB ==");
+        System.out.println("==============");
+        cc.HSVtoRGB();
+        cc.print();
+
+        System.out.println("==============");
+        System.out.println("== Random RGB to HSV ==");
+        System.out.println("==============");
+
+        //Show Random RGB to HSV
+        Random rand = new Random();
+        int[] redArray = new int[16];
+        int[] greenArray = new int[16];
+        int[] blueArray = new int[16];
+
+        for(int i=0; i < 16; i++){
+            redArray[i] = rand.nextInt(256);
+            greenArray[i] = rand.nextInt(256);
+            blueArray[i] = rand.nextInt(256);
+        }
+
+        ColorConverter ccRandomRGB = new ColorConverter(4, 4, 1, redArray, greenArray, blueArray);
+        ccRandomRGB.RGBtoHSV();
+        ccRandomRGB.print();
+
+        System.out.println("==============");
+        System.out.println("== Random HSV to RGB ==");
+        System.out.println("==============");
+
+        //SHow Random HSV to RGB
+        int[] hueArray = new int[16];
+        int[] saturationArray = new int[16];
+        int[] brightnessArray = new int[16];
+
+        for(int i=0; i < 16; i++){
+            hueArray[i] = rand.nextInt(360);
+            saturationArray[i] = rand.nextInt(101);
+            brightnessArray[i] = rand.nextInt(101);
+        }
+
+        ColorConverter ccRandomHSV = new ColorConverter(4, 4, 2, hueArray, saturationArray, brightnessArray);
+        ccRandomHSV.HSVtoRGB();
+        ccRandomHSV.print();
+
+        //Menu
+        Scanner s = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter Conversion Choice (1 for RGB-to-HSV or 2 for HSV-to-RGB)");
+
+        int type = s.nextInt();
+        if(type != 1 && type != 2){
+            System.out.println("Invalid entry");
+            System.exit(0);
+        }
+
+        System.out.println("Enter Color Array Size between 1 and 16");
+        int size = s.nextInt();
+        if(type == 0 || size > 16){
+            System.out.println("Invalid range");
+            System.exit(0);
+        }
+
+        int[] x = new int[size];
+        int[] y = new int[size];
+        int[] z = new int[size];
+
+        System.out.println("Enter location of your data file");
+        String dataFileLoc = s.next();
+        try {
+            File f = new File(dataFileLoc);
+            System.out.println(f.getAbsolutePath());
+            Scanner dataScanner = new Scanner(f);
+            for (int i=0;i<size;i++){
+                x[i]=dataScanner.nextInt();
+                if(type ==1 && (x[i] <0 || x[i] > 255)){
+                    System.out.println("Invalid value for Red. Range 0-255");
+                    System.exit(0);
+                } else if (type ==2 && (x[i] <0 || x[i] >= 360)){
+                    System.out.println("Invalid value for Hue. Range 0-359 degrees");
+                    System.exit(0);
+                }
+            }
+
+            for (int i=0;i<size;i++){
+                y[i]=dataScanner.nextInt();
+                if(type ==1 && (y[i] <0 || y[i] > 255)){
+                    System.out.println("Invalid value for Green . Range 0-255");
+                    System.exit(0);
+                } else if (type ==2 && (y[i] <0 || y[i] > 100)){
+                    System.out.println("Invalid value for Saturation. Range 0-100 %");
+                    System.exit(0);
+                }
+            }
+
+            for (int i=0;i<size;i++){
+                z[i]=dataScanner.nextInt();
+                if(type ==1 && (z[i] <0 || z[i] > 255)){
+                    System.out.println("Invalid value for Blue. Range 0-255");
+                } else if (type ==2 && (z[i] <0 || z[i] > 100)){
+                    System.out.println("Invalid value for Brightness. Range 0-100 %");
+                }
+
+            }
+        }
+        catch(Exception e){
+            System.out.println("Something went wrong: " + e.toString());
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+
+        System.out.println("==============");
+        System.out.println("== User Input  ==");
+        System.out.println("==============");
+
+        ColorConverter userInputColors = new ColorConverter(size/4, 4, type, x, y, z);
+        if(type==1){
+            userInputColors.RGBtoHSV();
         } else {
-            return d3;
+            Color[][] rectColors = userInputColors.HSVtoRGB();
+            ColorConverterDriver rects = new ColorConverterDriver();
+            rects.rectColors = rectColors;
+            JFrame frame = new JFrame("Rectangles");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.add(rects);
+            frame.setSize(250, 250);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+
         }
-
-    }
-
-    private double getMinimum(double d1, double d2, double d3) {
-        if (d1 <= d2 && d1 <= d3) {
-            return d1;
-        } else if (d2 <= d1 && d2 <= d3) {
-            return d2;
-        } else {
-            return d3;
-        }
-
-    }
-
-    private double calculateHue(double r, double g, double b, double max, double min){
-
-        double rp = r/MAX_COLOR;
-        double gp = g/MAX_COLOR;
-        double bp = b/MAX_COLOR;
-
-        max = getMaximum(rp,gp,bp);
-        min = getMinimum(rp,gp,bp);
-
-        double delta =  max- min;
-
-        if(delta==0){
-            return 0;
-        }
-        double hue = 0;
-        if(max == rp){
-            hue = 60*(((gp-bp)/delta)%6);
-        }
-        if (max == gp){
-            hue= 60*((bp-rp)/delta +2);
-        }
-        if (max == bp){
-            hue =  60*((rp-gp)/delta +4);
-        }
-
-        if(hue<0 ){
-            hue += 360;
-        } else if (hue>=360){
-            hue -= 360;
-        }
-        return hue;
-
-
-    }
-
-
-
-    private double calculateSaturation(double r, double g, double b, double max, double min){
-
-        double rp = r/(double)MAX_COLOR;
-        double gp = r/(double)MAX_COLOR;
-        double bp = r/(double)MAX_COLOR;
-
-
-        double delta =  max- min;
-
-        if(max ==0){
-            return 0;
-        }
-        if (max !=0){
-            return delta/max*100;
-        }
-        return -1;
-
-
-    }
-
-    private double calculateBrightness(){
-
-
-        double rp = this.red/(double)MAX_COLOR;
-        double gp = this.green/(double)MAX_COLOR;
-        double bp = this.blue/(double)MAX_COLOR;
-
-
-
-        return  getMaximum(rp,gp,bp)*100;
-
-
-    }
-
-    public Color RGBtoHSV(){
-        double r = this.red;
-        double g = this.green;
-        double b = this.blue;
-        double max = getMaximum(r,g,b);
-        double min= getMinimum(r,g,b);
-        hue = (int) calculateHue(r,g,b,max,min);
-        saturation = (int) calculateSaturation(r,g,b,max,min);
-        brightness = (int) calculateBrightness();
-
-        //return new Color(2,hue, saturation, brightness);
-        return this;
-    }
-
-    public Color HSVtoRGB(){
-        double h= this.hue;
-        double s = this.saturation/(double)100;
-        double v = this.brightness/(double)100;
-        double c = v * s;
-        double y = (h/60)%2 - 1;
-        if (y < 0){
-            y = y * -1;
-        }
-        double x = c * (1 - y );
-
-
-        double m = v - c;
-        double rp = 0, gp=0, bp=0;
-
-        if(h >=0 && h <60){
-            rp = c;
-            gp=x;
-            bp=0;
-        }
-        else if (h >=60 && h < 120){
-            rp =x;
-            gp =c;
-            bp =0;
-        }
-        else if (h >=120 && h <180){
-            rp =0;
-            gp =c;
-            bp =x;
-        }
-        else if (h >=180 && h <240){
-            rp =0;
-            gp =x;
-            bp =c;
-        }
-        else if (h >= 240 && h <300){
-            rp =x;
-            gp =0;
-            bp =c;
-        }
-        else if (h  >=300 && h <360){
-            rp =c;
-            gp =0;
-            bp =x;
-        }
-        double rt = (rp +m)*MAX_COLOR;
-        if(rt>255){
-            red = (int)rt;
-        } else {
-            //if (rt * 10 % 10 != 0){
-             //   red = (int)rt +1;
-            //}
-            red = (int)(rt + 0.99);
-        }
-
-        double gt = (gp +m)*MAX_COLOR;
-        if(gt>255){
-            green = (int)gt;
-        } else {
-            //if (gt * 10 % 10 != 0){
-             //   green = (int)gt +1;
-            //}
-            green = (int)(gt + 0.99);
-        }
-
-        double bt = (bp +m)*MAX_COLOR;
-        if(bt>255){
-            blue = (int)bt;
-        } else {
-            //if (bt * 10 % 10 != 0){
-            //    blue = (int)bt +1;
-            //}
-            blue = (int)(bt +0.99);
-        }
-
-        return this;
-        //return new Color(1, red, green, blue);
-    }
-
-
-    public void printColor()
-    {
-        char c = 176;
-        System.out.printf("%10s%3d%2s%3d%2s%3d%2s", "\tRGB = (" ,
-                red , ", " , green , ", " , blue , ")");
-        System.out.printf("%10s%3d%2s%3d%2s%3d%2s", "HSV = (" , hue
-                , (""+ c+", ") , saturation , "%, " , brightness ,
-                "%)\n");
-    }
-
-
-
-    public static void main (String args[]){
-        System.out.println("TESTING");
-        //Color c = new Color(1,0,128,128);
-        //c.RGBtoHSV();
-        //c.printColor();
-
-        Color c = new Color (2,0,0,75);
-        c.HSVtoRGB();
-        c.printColor();
-
+        userInputColors.print();
     }
 }
