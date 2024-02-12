2afaefd57d6a519dc391b3a746bfbd7b05d5ee3b
esha0612 <73080687+esha0612@users.noreply.github.com>
2024-02-11 18:34:40 -0800

    Update ColorConvertor.java

--- ColorConvertor.java
+++ ColorConvertor.java
@@ -3,9 +3,20 @@
 import java.awt.*;
 
 public class ColorConverter {
-    //Add instance variables here
+    //Instance variable declaring 2D array myColors holding Color objects
     private Color[][] myColors;
-
+    /**
+	   * Constructor creates a 2-D array of the given number of rows and columns
+	   * Constructor also populates the array by the given three arrays: first, second, third
+	   * @author - Esha Shivakumar
+	   * @param rows - number of rows
+	   * @param cols - number of columns
+	   * @param type - type of array (RGB/HSV integers)
+	   * @param first - array of red integers or hue integers
+	   * @param second - array of green integers or saturation integers
+	   * @param third - array of blue integers or value integers
+	   * 
+	   */
     public ColorConverter(int rows, int cols, int type, int[] first, int[] second, int[] third)
     {
         myColors = new Color[rows][cols];
@@ -17,7 +28,12 @@ public class ColorConverter {
         }
     }
 
-
+    /**
+	   * Calls the RGBtoHSV() method from color class to convert HSV to RGB
+	   * @author - Esha Shivakumar
+	   * @return - 2D array of color objects (myColors) where values are manipulated to HSV values 
+	   * 
+	   */
     public Color[][] RGBtoHSV() {
         for (int i = 0; i < myColors.length; i++) {
             for (int j = 0; j < myColors[i].length; j++) {
@@ -26,6 +42,12 @@ public class ColorConverter {
         }
         return myColors;
     }
+    
+    /**
+	   * Calls the HSVtoRGB() method from color class to convert HSV to RGB
+	   * @author - Esha Shivakumar
+	   * @return - 2D array of color objects (myColors) where values are manipulated to RGB values 
+	   */
     public Color[][] HSVtoRGB() {
         for (int i = 0; i < myColors.length; i++) {
             for (int j = 0; j < myColors[i].length; j++) {
@@ -35,6 +57,11 @@ public class ColorConverter {
         return myColors;
     }
 
+    
+    /**
+	   * Prints the 2D array of color objects (myColors) by calling printColor() method from Color class
+	   * @author - Esha Shivakumar	   
+	   */
     public void print() {
         for (int i = 0; i < myColors.length; i++) {
             for (int j = 0; j < myColors[i].length; j++) {
